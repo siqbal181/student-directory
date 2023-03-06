7c684406d1f7261314c827f3342a7918de6bdf1b
Sidra Iqbal <siqbal@nmplondon.com>
2023-03-06 14:03:13 +0000

    Add a method to dynamically input students

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-  ]
+def input_students
+    puts "Please enter the names of students"
+    puts "To finish, just hit return twice"
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        # get another name from the user
+        name = gets.chomp
+    end
+    students
+end
 
 def print_header
     puts "The students of Villains Academy"
@@ -19,7 +19,7 @@ end
 
 def print(students)
     students.each do |student|
-        puts "#{student[:name]} #{student[:cohort]} cohort)"
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
@@ -28,6 +28,7 @@ def print_footer(names)
 end
 
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
