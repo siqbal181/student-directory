af3fe5c96d71e644caac732280092b0f2aabbc65
Sidra Iqbal <siqbal@nmplondon.com>
2023-03-06 14:07:33 +0000

    Add a new file for exercises

---
+++ directory-exercises.rb
@@ -0,0 +1,34 @@
+def input_students
+    puts "Please enter the names of students"
+    puts "To finish, just hit return twice"
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        # get another name from the user
+        name = gets.chomp
+    end
+    students
+end
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
+
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer(names)
+    puts "Overall we have #{names.count} great students"
+end
+
+# nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
