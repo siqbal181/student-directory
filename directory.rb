001a0446a743c7f4584df09e55831148a43bd596
Sidra Iqbal <siqbal@nmplondon.com>
2023-03-06 10:22:59 +0000

    Added some comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first print a list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally print the total number of students
 print "Overall, we have "
+# print() does not add a new line character
 print 11
 print " great students"
